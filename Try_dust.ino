ad591ee0d13f3f0979816637dc22e27faffbc437
Aweenagua <faweenagua@gmail.com>
2022-08-22 18:10:59 +0000

    Update Try_dust.ino

--- Try_dust.ino
+++ Try_dust.ino
@@ -11,7 +11,7 @@ void setup()
 {
   Serial1.begin(9600);
   Serial.begin(9600);
-  Serial.println("PMSA003I Air Quality Sensor");
+  //Serial.println("PMSA003I Air Quality Sensor");
   delay(2000);
 }
  
@@ -19,9 +19,9 @@ void loop()
 {
   if (pms.read(data))
   {
-    Serial.println("Dust Concentration");
-    Serial.println("PM1.0 :" + String(data.PM_AE_UG_1_0) + "(ug/m3)");
-    Serial.println("PM2.5 :" + String(data.PM_AE_UG_2_5) + "(ug/m3)");
+    // Serial.println("Dust Concentration");
+    // Serial.println("PM1.0 :" + String(data.PM_AE_UG_1_0) + "(ug/m3)");
+    // Serial.println("PM2.5 :" + String(data.PM_AE_UG_2_5) + "(ug/m3)");
     Serial.println("PM10  :" + String(data.PM_AE_UG_10_0) + "(ug/m3)");
     delay(5000);
   }
